68f873ec384be72d8ae5eaebee63cd788167dc5e
Jon2137 <67341679+Jon2137@users.noreply.github.com>
2021-05-24 11:55:41 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+$('.sliders').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+  $('.js-add-slide').on('click', function() {
+    slideIndex++;
+    $('.add-remove').slick('slickAdd','<div><h3>' + slideIndex + '</h3></div>');
+  });
+  
+  $('.js-remove-slide').on('click', function() {
+    $('.add-remove').slick('slickRemove',slideIndex - 1);
+    if (slideIndex !== 0){
+      slideIndex--;
+    }
+  });
+  $$('.caixas').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+  $('.js-add-slide').on('click', function() {
+    slideIndex++;
+    $('.add-remove').slick('slickAdd','<div><h3>' + slideIndex + '</h3></div>');
+  });
+  
+  $('.js-remove-slide').on('click', function() {
+    $('.add-remove').slick('slickRemove',slideIndex - 1);
+    if (slideIndex !== 0){
+      slideIndex--;
+    }
+  });
